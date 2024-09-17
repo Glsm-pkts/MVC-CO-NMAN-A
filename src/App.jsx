8a8c9a8b0d1a1e1ce42d1c5f6b23dc02606cd21c
Büşra Gülsüm PEKTAŞ <158344482+Glsm-pkts@users.noreply.github.com>
2024-09-17 19:00:43 +0300

    readme duzenlendi

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './view/LoginPage';
 import HeaderView from './view/HeaderView';
 import MainPageController from './controller/MainPageController';
-import axios from 'axios';
 import DetailPageController from './controller/DetailPageController';
+import axios from 'axios';
 
-axios.defaults.baseURL = "https://api.coincap.io/v2/";
+// Doğrudan CoinCap API'sini HTTPS ile kullanıyoruz
+axios.defaults.baseURL = "https://api.coincap.io/v2";
+
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <HeaderView/>
-
-      <Routes>
-<Route path="/" element={<LoginPage/>}/>
-<Route path='/home' element={<MainPageController/>}/>
-<Route path='/coin/:id' element={<DetailPageController/>}/>
-      </Routes>
+        <HeaderView />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<MainPageController />} />
+          <Route path="/coin/:id" element={<DetailPageController />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
--- src/controller/MainPageController.jsx
+++ src/controller/MainPageController.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import MainPageView from "../view/MainPageView";
 import axios from "axios";
-import {  useNavigate, useSearchParams } from 
-"react-router-dom";
-import LoadMoreController from "../controller/LoadMoreController"
+import { useEffect, useState } from "react";
+import MainPageView from "../view/MainPageView";   
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+axios.defaults.baseURL = "https://api.coincap.io/v2"; // Proxy kullanmadan doğrudan API
 
 const MainPageController = () => {
   const [coins, setCoins] = useState([]);
@@ -12,25 +12,21 @@ const MainPageController = () => {
   const navigate = useNavigate();
   const page = params.get("page");
 
-  
-    useEffect(() => {
-        axios
-          .get(`/assets?limit=15&offset=${page ? page : 1}`)
-          .then((res) => {
-            setCoins(coins.concat(res.data.data));
-            if (!popular) {
-              setPopular(res.data.data.slice(0, 3));
-            }
-          })
-          .catch((err) => console.log(err));
-      }, [params]);
-    
+  useEffect(() => {
+    axios
+      .get(`/assets?limit=15&offset=${page ? page : 1}`)
+      .then((res) => {
+        setCoins(coins.concat(res.data.data));
+        if (!popular) {
+          setPopular(res.data.data.slice(0, 3));
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [params]);
+
   return (
-    
-      <MainPageView popular={popular} coins={coins} navigate={navigate} />
-    
+    <MainPageView popular={popular} coins={coins} navigate={navigate} />
   );
-  
 };
 
 export default MainPageController;
